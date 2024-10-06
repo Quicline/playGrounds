0a3c0574f4faadf5f8ec16645c711ef074e2fd11
Quicline <armando17171771@gmail.com>
2024-10-06 19:49:21 -0400

    Initial Commit

---
+++ celsiusToFahrenheit.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+//var greeting = "Hello, playground"
+
+let celsius: Double = 30.0
+// celsius to Fahrenheit formula((celsius * 9) / 5) + 32
+let Fahrenheit: Double = ((celsius * 9) / 5) + 32
+
+print("\(Fahrenheit)\("°") Fahrenheit")
