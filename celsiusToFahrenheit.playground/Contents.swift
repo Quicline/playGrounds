527213d476df641730b6b4d0611d380bcfe1fd39
Quicline <armando17171771@gmail.com>
2024-10-06 20:01:13 -0400

    changing 30 to 32 lol

--- celsiusToFahrenheit.playground/Contents.swift
+++ celsiusToFahrenheit.playground/Contents.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 //var greeting = "Hello, playground"
 
-let celsius: Double = 30.0
+let celsius: Double = 32.0
 // celsius to Fahrenheit formula((celsius * 9) / 5) + 32
 let Fahrenheit: Double = ((celsius * 9) / 5) + 32
 
