617fc49a7af00dc90224922bd2982d8690a81f76
Armando Francisco <43098740+Quicline@users.noreply.github.com>
2024-10-16 04:39:11 -0400

    Adding celsius convertion

--- celsiusToFahrenheit.playground/Contents.swift
+++ celsiusToFahrenheit.playground/Contents.swift
@@ -3,7 +3,13 @@ import Cocoa
 //var greeting = "Hello, playground"
 
 let celsius: Double = 32.0
-// celsius to Fahrenheit formula((celsius * 9) / 5) + 32
-let Fahrenheit: Double = ((celsius * 9) / 5) + 32
+// Fahrenheit to celsius formula((fahrenheit - 32) * 5) / 9
+let celsiusConvertion: Double = ((fahrenheit - 32) * 5) / 9
 
-print("\(Fahrenheit)\("°") Fahrenheit")
+let fahrenheit: Double = 90.0
+// celsius to Fahrenheit formula((celsius * 9) / 5) + 32
+let fahrenheitConvertion: Double = ((celsius * 9) / 5) + 32
+
+
+print("\(fahrenheitConvertion)\("°") Fahrenheit")
+print("\(celsiusConvertion)\("°") Celsius")
